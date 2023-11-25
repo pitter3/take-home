db0f64b1bb756dd31013282239d003817b380ce7
Patrick Eitel <patrickeitel12@gmail.com>
2023-11-25 12:16:01 -0800

    create all components and add mockdata

---
+++ src/mockData.js
@@ -0,0 +1,47 @@
+const mockData = {
+  "status": "ok",
+  "totalResults": 36,
+  "articles": [
+    {
+      "source": {
+        "id": "financial-times",
+        "name": "Financial Times"
+      },
+      "author": "Harriet Clarfelt, Colby Smith",
+      "title": "Bonds rally as US jobs growth slows more than forecast - Financial Times",
+      "description": "Posts created last month were barely half of September’s total, in sign that world’s biggest economy is slowing",
+      "url": "https://www.ft.com/content/52e76c22-3e0a-420b-8fdb-b5594d74cba8",
+      "urlToImage": "https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fwww.ft.com%2F__origami%2Fservice%2Fimage%2Fv2%2Fimages%2Fraw%2Fhttps%253A%252F%252Fd1e00ek4ebabms.cloudfront.net%252Fproduction%252F7ee2195f-508d-44bc-a83f-a5935b7b8dae.jpg%3Fsource%3Dnext-article%26fit%3Dscale-down%26quality%3Dhighest%26width%3D700%26dpr%3D1?source=next-opengraph&fit=scale-down&width=900",
+      "publishedAt": "2023-11-03T13:38:15Z",
+      "content": "Jobs growth in the US slowed sharply in October in an indication that the world's largest economy is starting to cool, reigniting a rally in government bonds.\nUS employers created 150,000 new posts l… [+3800 chars]"
+    },
+    {
+      "source": {
+        "id": null,
+        "name": "[Removed]"
+      },
+      "author": null,
+      "title": "[Removed]",
+      "description": "[Removed]",
+      "url": "https://removed.com",
+      "urlToImage": null,
+      "publishedAt": "1970-01-01T00:00:00Z",
+      "content": "[Removed]"
+    },
+    {
+      "source": {
+        "id": null,
+        "name": "Fox Business"
+      },
+      "author": "FOXBusiness",
+      "title": "Jeff Bezos to leave Seattle for Miami nearly 30 years after founding Amazon - Fox Business",
+      "description": "Jeff Bezos is leaving Seattle after nearly 30 years and moving to Miami. Bezos posted a video of him giving a tour of Amazon's first office in Seattle.",
+      "url": "https://www.foxbusiness.com/business-leaders/jeff-bezos-leave-seattle-miami-nearly-30-years-after-founding-amazon",
+      "urlToImage": "https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2022/10/0/0/be6a8ea9-bezos-sanchez.jpg?ve=1&tl=1",
+      "publishedAt": "2023-11-03T13:26:03Z",
+      "content": "Amazon founder Jeff Bezos has announced that he is leaving Seattle after nearly 30 years and moving back to the warmth of Miami.\nBezos, 59, announced he is ditching the birthplace of the e-commerce … [+2825 chars]"
+    }
+  ]
+};
+
+export default mockData;
